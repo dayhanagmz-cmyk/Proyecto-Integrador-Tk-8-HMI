cf30787c857fe28fe81f6b81d211bb8d40e81f94
Gómez Hernández Angelica Dayhana <dayhanagmzhdz@gmail.com>
2026-05-14 17:28:05 -0600

    Create codigo_librerias.ino

---
+++ codigo_librerias.ino
@@ -0,0 +1,6 @@
+lib_deps para indicarle al compilador qué librerías externas necesita el proyecto.
+
+ib_deps =
+    adafruit/Adafruit BMP280 Library @ ^2.6.8
+    adafruit/DHT sensor library @ ^1.4.6
+    adafruit/Adafruit Unified Sensor @ ^1.1.14
